1a10cbf46b6927b36fbb25b59c3fbb9a05dbbd34
Katerina <katerinaldg@gmail.com>
2017-10-24 13:31:02 +0100

    Applied refactoring to whole program

--- src/main/java/RomanNumerals.java
+++ src/main/java/RomanNumerals.java
@@ -2,23 +2,14 @@ public class RomanNumerals {
 
   public static String convert(int number) {
     String roman = "";
-    int[] arabic_numbers = {10};
-    String[] roman_numerals = {"X"};
+    int[] arabic_numbers = {10, 5, 1};
+    String[] roman_numerals = {"X", "V", "I"};
 
     for (int i = 0; i < arabic_numbers.length; i++ ) {
       while (number >= arabic_numbers[i]) {
         number -= arabic_numbers[i];
         roman += roman_numerals[i];
       }
-    }
-    while (number >= 5) {
-      number -= 5;
-      roman += "V";
-    }
-
-    while (number < 4 && number > 0) {
-      number -= 1;
-      roman += "I";
     }
     return roman;
   }
